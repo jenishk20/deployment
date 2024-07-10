357ecfebb98238cdc93b55979c202c8508033944
Jenish Kothari <kotharijenish2001@gmail.com>
2024-07-10 11:28:53 -0400

    Deployment Settings for the project.

---
+++ scripts/after_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Clone the frontend and backend repositories
+cd /home/ec2-user/deployment
+git clone https://github.com/jenishk20/job-fit-ai-frontend
+git clone https://github.com/jenishk20/job-fit-ai-backend
---
+++ scripts/start_server.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/deployment
+docker-compose up --build -d
